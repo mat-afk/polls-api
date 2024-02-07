585166ee139ff532cf1747d98f506dbb63ac7d43
Mateus <mateusc.ramos@outlook.com>
2024-02-06 21:34:57 -0300

    add get poll route

---
+++ src/http/routes/get-poll.ts
@@ -0,0 +1,30 @@
+import { FastifyInstance } from "fastify";
+import { prisma } from "../../lib/prisma";
+import { z } from "zod";
+import { truncate } from "fs";
+
+export async function getPoll(app: FastifyInstance) {
+  app.get("/polls/:pollId", async (request, reply) => {
+    const getPollParams = z.object({
+      pollId: z.string().uuid(),
+    });
+
+    const { pollId } = getPollParams.parse(request.params);
+
+    const poll = await prisma.poll.findUnique({
+      where: {
+        id: pollId,
+      },
+      include: {
+        options: {
+          select: {
+            id: true,
+            title: true,
+          },
+        },
+      },
+    });
+
+    return reply.send({ poll });
+  });
+}
